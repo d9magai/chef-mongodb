46639f97f83b26db83c7ad145f35b970d6ee5a1e
d9magai <kumagai@sencorp.co.jp>
2014-08-28 19:28:52 +0900

    install from archive

--- attributes/sysconfig.rb
+++ attributes/sysconfig.rb
@@ -1,6 +1,10 @@
 include_attribute 'mongodb::default'
 
-default['mongodb']['sysconfig']['DAEMON'] = '/usr/bin/$NAME'
+if node[:mongodb][:from_archive] then
+  default['mongodb']['sysconfig']['DAEMON'] = "#{node['mongodb']['from_archive']['install_path']}/mongodb/bin/$NAME"
+else
+  default['mongodb']['sysconfig']['DAEMON'] = '/usr/bin/$NAME'
+end
 default['mongodb']['sysconfig']['DAEMON_USER'] = node['mongodb']['user']
 default['mongodb']['sysconfig']['DAEMON_OPTS'] = "--config #{node['mongodb']['dbconfig_file']}"
 default['mongodb']['sysconfig']['CONFIGFILE'] = node['mongodb']['dbconfig_file']
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,4 +1,35 @@
 if node[:mongodb][:from_archive] then
+  archive_filename = File.basename node[:mongodb][:from_archive][:url]
+  remote_file "#{Chef::Config[:file_cache_path]}/#{archive_filename}" do
+    source node[:mongodb][:from_archive][:url]
+  end
+
+  execute "file unzipping" do
+    command "tar xf #{Chef::Config[:file_cache_path]}/#{archive_filename} -C #{node[:mongodb][:from_archive][:install_path]}"
+  end
+
+  link "#{node[:mongodb][:from_archive][:install_path]}/mongodb" do
+    to File.basename(archive_filename, '.*')
+  end
+
+  user node['mongodb']['user'] do
+    supports :manage_home => false
+    action   [:create]
+  end
+
+  directory "/var/lib/mongodb" do
+    owner node['mongodb']['user']
+    group node['mongodb']['user']
+    mode "0755"
+    action :create
+  end
+
+  directory "/var/run/mongo" do
+    owner node['mongodb']['user']
+    group node['mongodb']['user']
+    mode "0755"
+    action :create
+  end
 else
   # install the 10gen repo if necessary
   include_recipe 'mongodb::10gen_repo' if %w(10gen mongodb-org).include?(node['mongodb']['install_method'])
