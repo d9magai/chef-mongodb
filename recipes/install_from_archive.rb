62a8b8991d64a3eb3cb6e55a5cb2961fb92090fe
d9magai <kumagai@sencorp.co.jp>
2014-08-29 11:26:11 +0900

    file isnt unzipping, if dir exists

--- recipes/install_from_archive.rb
+++ recipes/install_from_archive.rb
@@ -3,12 +3,14 @@ remote_file "#{Chef::Config[:file_cache_path]}/#{archive_filename}" do
   source node[:mongodb][:from_archive][:url]
 end
 
+archive_basename = File.basename(archive_filename, '.*')
 execute "file unzipping" do
   command "tar xf #{Chef::Config[:file_cache_path]}/#{archive_filename} -C #{node[:mongodb][:from_archive][:install_path]}"
+  not_if "test -e #{node[:mongodb][:from_archive][:install_path]}/#{archive_basename}"
 end
 
 link "#{node[:mongodb][:from_archive][:install_path]}/mongodb" do
-  to File.basename(archive_filename, '.*')
+  to archive_basename
 end
 
 user node['mongodb']['user'] do
